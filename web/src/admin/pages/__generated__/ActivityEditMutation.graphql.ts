b2595a84258942f25dafd6ef2fbc148cb044fde0
Sam Dunster <me@sdunster.com>
2026-07-23 15:52:50 +1000

    admin site: add period comment field
    Summary: so we can edit a period's comment manually
    
    Test Plan: tested in dev

--- web/src/admin/pages/__generated__/ActivityEditMutation.graphql.ts
+++ web/src/admin/pages/__generated__/ActivityEditMutation.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<9683e8fcb59c207ecd8c04881bdff5e3>>
+ * @generated SignedSource<<928212ad50e1f4949bce31a135ced2a6>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -11,6 +11,7 @@
 import { ConcreteRequest } from 'relay-runtime';
 export type ActivityEditMutation$variables = {
   categoryId: string;
+  comment?: string | null | undefined;
   endTime: number;
   id: string;
   startTime: number;
@@ -21,6 +22,7 @@ export type ActivityEditMutation$data = {
       readonly id: string;
       readonly name: string;
     } | null | undefined;
+    readonly comment: string | null | undefined;
     readonly endTime: number | null | undefined;
     readonly id: string;
     readonly startTime: number;
@@ -40,26 +42,31 @@ var v0 = {
 v1 = {
   "defaultValue": null,
   "kind": "LocalArgument",
-  "name": "endTime"
+  "name": "comment"
 },
 v2 = {
   "defaultValue": null,
   "kind": "LocalArgument",
-  "name": "id"
+  "name": "endTime"
 },
 v3 = {
   "defaultValue": null,
   "kind": "LocalArgument",
-  "name": "startTime"
+  "name": "id"
 },
 v4 = {
+  "defaultValue": null,
+  "kind": "LocalArgument",
+  "name": "startTime"
+},
+v5 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
   "name": "id",
   "storageKey": null
 },
-v5 = [
+v6 = [
   {
     "alias": null,
     "args": [
@@ -67,6 +74,11 @@ v5 = [
         "kind": "Variable",
         "name": "categoryId",
         "variableName": "categoryId"
+      },
+      {
+        "kind": "Variable",
+        "name": "comment",
+        "variableName": "comment"
       },
       {
         "kind": "Variable",
@@ -89,7 +101,7 @@ v5 = [
     "name": "updatePeriodTimeCategory",
     "plural": false,
     "selections": [
-      (v4/*: any*/),
+      (v5/*: any*/),
       {
         "alias": null,
         "args": null,
@@ -107,12 +119,19 @@ v5 = [
       {
         "alias": null,
         "args": null,
+        "kind": "ScalarField",
+        "name": "comment",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
         "concreteType": "Category",
         "kind": "LinkedField",
         "name": "category",
         "plural": false,
         "selections": [
-          (v4/*: any*/),
+          (v5/*: any*/),
           {
             "alias": null,
             "args": null,
@@ -133,38 +152,40 @@ return {
       (v0/*: any*/),
       (v1/*: any*/),
       (v2/*: any*/),
-      (v3/*: any*/)
+      (v3/*: any*/),
+      (v4/*: any*/)
     ],
     "kind": "Fragment",
     "metadata": null,
     "name": "ActivityEditMutation",
-    "selections": (v5/*: any*/),
+    "selections": (v6/*: any*/),
     "type": "MutationRoot",
     "abstractKey": null
   },
   "kind": "Request",
   "operation": {
     "argumentDefinitions": [
-      (v2/*: any*/),
       (v3/*: any*/),
-      (v1/*: any*/),
-      (v0/*: any*/)
+      (v4/*: any*/),
+      (v2/*: any*/),
+      (v0/*: any*/),
+      (v1/*: any*/)
     ],
     "kind": "Operation",
     "name": "ActivityEditMutation",
-    "selections": (v5/*: any*/)
+    "selections": (v6/*: any*/)
   },
   "params": {
-    "cacheID": "a95696840fada09bed5a3e688ccb5f04",
+    "cacheID": "95867046619766d85abb34222f76f8b3",
     "id": null,
     "metadata": {},
     "name": "ActivityEditMutation",
     "operationKind": "mutation",
-    "text": "mutation ActivityEditMutation(\n  $id: ID!\n  $startTime: Int!\n  $endTime: Int!\n  $categoryId: ID!\n) {\n  updatePeriodTimeCategory(id: $id, startTime: $startTime, endTime: $endTime, categoryId: $categoryId) {\n    id\n    startTime\n    endTime\n    category {\n      id\n      name\n    }\n  }\n}\n"
+    "text": "mutation ActivityEditMutation(\n  $id: ID!\n  $startTime: Int!\n  $endTime: Int!\n  $categoryId: ID!\n  $comment: String\n) {\n  updatePeriodTimeCategory(id: $id, startTime: $startTime, endTime: $endTime, categoryId: $categoryId, comment: $comment) {\n    id\n    startTime\n    endTime\n    comment\n    category {\n      id\n      name\n    }\n  }\n}\n"
   }
 };
 })();
 
-(node as any).hash = "a22026eeca1cba3a15e3733d6ddb0b82";
+(node as any).hash = "60325151a5305515c9c2a345508404cb";
 
 export default node;
