a0bdc2549849034e782db200855bdfd29fa9e9df
Sam Dunster <me@sdunster.com>
2026-07-24 10:06:55 +1000

    scan: GPU compositing transitions for sliding views
    apparently this might look smoother

--- web/src/styles.ts
+++ web/src/styles.ts
@@ -2,10 +2,10 @@ import { tw } from "./lib/tw";
 
 export type ScreenPosition = "offLeft" | "center" | "offRight";
 
-export const scanView = tw`absolute w-full px-2.5 transition-[left] duration-500 ease-in-out`;
+export const scanView = tw`absolute left-0 w-full px-2.5 transition-transform duration-500 ease-in-out`;
 
 export const scanViewPosition: Record<ScreenPosition, string> = {
-  offLeft: tw`-left-full`,
-  center: tw`left-0`,
-  offRight: tw`left-full`,
+  offLeft: tw`-translate-x-full`,
+  center: tw`translate-x-0`,
+  offRight: tw`translate-x-full`,
 };
